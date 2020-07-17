c45dbd1fcbcbae9c985ab0302ab2038cba45ad94
Francisco Delgado <franciscodelgado503@gmail.com>
2020-07-17 12:48:05 -0500

    Removed crispy_forms temporarily
    Removed crispy_forms temporarily

--- tkms/settings.py
+++ tkms/settings.py
@@ -39,11 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'training.apps.TrainingConfig',
-    'crispy_forms',
+    # 'crispy_forms',
     'rest_framework',
 ]
 
-CRISPY_TEMPLATE_PACK = "bootstrap4"
+# CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
