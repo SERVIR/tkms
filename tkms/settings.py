b0fe6880b4099bab58dc659d054e070d8083b3bb
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2021-04-12 14:55:35 -0500

    fixed image ubload via ImgeField

--- tkms/settings.py
+++ tkms/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(Path(__file__).resolve(strict=True).parents[1], 'staticfiles/')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 ##################
 # AUTHENTICATION #
 ##################
